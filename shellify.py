687639f38f3bfe678ea48f65b8a141a681d4870c
Alex Gunkov <alex@carbonre.com>
2023-10-28 16:49:26 +0100

    cosmetics

--- shellify.py
+++ shellify.py
@@ -2,6 +2,21 @@
 import openai
 import os
 import argparse
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+
+def api_call(prompt, api_key):
+    openai.api_key = api_key
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are an app which outputs shell oneliners by given input.\
+             output should be pure oneliner with no comments or newlines"},
+            {"role": "user", "content": prompt},
+        ]
+    )
+    return response
 
 def shellify(prompt):
     api_key = os.getenv("OPENAI_API_KEY")
@@ -9,27 +24,18 @@ def shellify(prompt):
         print("Error: OPENAI_API_KEY environment variable not set.")
         return
     
-    openai.api_key = api_key
-    
-    response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are app which outputs shell oneliners by given input.\
-         output should be pure oneliner with no comments or newlines"},
-        {"role": "user", "content": prompt},
-        # {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        # {"role": "user", "content": "Where was it played?"}
-        ]
-    )
-    # Generate a shell oneliner using OpenAI GPT API
-
-    # response = openai.Completion.create(
-    #     engine="text-davinci",
-    #     prompt=f"Generate a shell oneliner for: {prompt}",
-    #     max_tokens=100
-    # )
+    with ThreadPoolExecutor() as executor:
+        future = executor.submit(api_call, prompt, api_key)
+        
+        # Print dots while waiting for the API call to complete
+        while not future.done():
+            print(".", end="", flush=True)
+            time.sleep(1)
+            
+        response = future.result()
     
     shell_command = response['choices'][0]['message']['content']
+    print()  # Add a newline after the dots
     return shell_command
 
 if __name__ == "__main__":
